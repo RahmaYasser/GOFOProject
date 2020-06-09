03ededaec47fded97be847f060561fb3cffe2a21
RahmaYasser <49435053+RahmaYasser@users.noreply.github.com>
2020-06-09 15:49:26 +0200

    Update Player.java

--- Player.java
+++ Player.java
@@ -4,8 +4,26 @@ import java.util.ArrayList;
 import java.util.Vector;
 
 public class Player extends User{
+    private String notification="You didn't receive messages yet";
+
+    public void setNotification(String notification) {
+        this.notification = notification;
+    }
+
+    public String getNotification() {
+        return notification;
+    }
+
+    public Player(String id, String name, String location, String email, String password, String phone, eWallet userEwallet) {
+        super(id, name, location, email, password, phone, userEwallet);
+        bookingHistory = new Vector<>();
+
+    }
+
+    public Team team;
     public Vector<Booking> bookingHistory;
     public Player(){
         bookingHistory = new Vector<>();
+        team = new Team();
     }
 }
