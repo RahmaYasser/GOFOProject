39007b0a7fd538942d9045f9ac8f92fb40edaf97
Eman Mahmoud <60985699+EmanMahm0ud@users.noreply.github.com>
2020-06-09 16:03:10 +0200

    Update Administrator.java

--- Administrator.java
+++ Administrator.java
@@ -7,32 +7,32 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.ArrayList;
+import java.util.Vector;
 
 import javax.swing.*;
 
 public class Administrator extends User implements ActionListener, MouseListener {
 	
-	String[] data = new String[20];
+	Vector<String> data = new Vector();
 	
 	JFrame frame = new JFrame();
-	JPanel panel = new JPanel();
-	JPanel panel2 = new JPanel();
-	JPanel panel3 = new JPanel();
-	JLabel label = new JLabel();
-	JLabel label2 = new JLabel("Select PG no.");
-	JLabel label3 = new JLabel("Location");
-	JLabel label4 = new JLabel("Size");
-	JLabel label5 = new JLabel("Description");
-	JLabel label6 = new JLabel("Price Per Hour");
-	JLabel label7 = new JLabel("Cancellation Period");
-	JLabel label8 = new JLabel("Slots");
+	JPanel adminHome = new JPanel();
+	JPanel adminAllPlaygrounds = new JPanel();
+	JPanel adminPlayground = new JPanel();
+	JLabel header = new JLabel();
+	JLabel PgNumLabel = new JLabel("Select PG no.");
+	JLabel locationLabel = new JLabel("Location");
+	JLabel sizeLabel = new JLabel("Size");
+	JLabel descriptionLabel = new JLabel("Description");
+	JLabel priceLabel = new JLabel("Price Per Hour");
+	JLabel cancellationLabel = new JLabel("Cancellation Period");
 	JLabel logout = new JLabel("logout");
-	JTextField text = new JTextField();
-	JButton button = new JButton("Playgrounds");
-	JButton button2 = new JButton("Go");
-	JButton button3 = new JButton("Delete");
-	JButton back1 = new JButton("<<");
-	JButton back2 = new JButton("<<");
+	JTextField PgNumText = new JTextField();
+	JButton aPlaygroundsButton = new JButton("Playgrounds");
+	JButton goButton = new JButton("Go");
+	JButton deleteButton = new JButton("Delete");
+	JButton adminBack1 = new JButton("<<");
+	JButton adminBack2 = new JButton("<<");
 	
 	Administrator() {
 		displayWelcome();
@@ -42,8 +42,8 @@ public class Administrator extends User implements ActionListener, MouseListener
 	 * This function builds the welcome page of the administrator
 	 */
 	private void displayWelcome() {
-		panel.setVisible(true);
-		panel2.setVisible(false);
+		adminHome.setVisible(true);
+		adminAllPlaygrounds.setVisible(false);
 		
 		frame.setTitle("GoFo");
 		frame.setSize(300, 450);
@@ -52,28 +52,28 @@ public class Administrator extends User implements ActionListener, MouseListener
 		frame.setResizable(false);
 		frame.setLocation(650, 200);
 		
-		button.setBackground(Color.ORANGE);
-		button.setForeground(Color.DARK_GRAY);
-		button.setBounds(80, 200, 130, 50);
+		aPlaygroundsButton.setBackground(new Color(255,134,0));
+		aPlaygroundsButton.setForeground(Color.WHITE);
+		aPlaygroundsButton.setBounds(80, 200, 130, 50);
 		
-		panel.setLayout(null);
-		panel.setBackground(Color.DARK_GRAY);
+		adminHome.setLayout(null);
+		adminHome.setBackground(new java.awt.Color(105,105,105));
 		
-		label.setForeground(Color.WHITE);
-		label.setText("WELCOME");
-		label.setFont (label.getFont ().deriveFont (23f));
-		label.setBounds(80, 50, 130, 50);
+		header.setForeground(Color.WHITE);
+		header.setText("WELCOME");
+		header.setFont (header.getFont ().deriveFont (23f));
+		header.setBounds(80, 50, 130, 50);
 		
 		logout.setForeground(Color.WHITE);
 		logout.setBounds(120, 300, 130, 50);
 		
-		frame.add(panel);
-		panel.add(button);
-		panel.add(label);
-		panel.add(logout);
+		frame.add(adminHome);
+		adminHome.add(aPlaygroundsButton);
+		adminHome.add(header);
+		adminHome.add(logout);
 		
 		logout.addMouseListener(this);
-		button.addActionListener(this);
+		aPlaygroundsButton.addActionListener(this);
 	}
 	
 	/**
@@ -96,12 +96,8 @@ public class Administrator extends User implements ActionListener, MouseListener
 	 */
 	public void deletePlayground(int index) {
 		AvailablePlaygrounds.playgrounds.remove(index-1);
-		for (int i = index-1 ; i < AvailablePlaygrounds.playgrounds.size() ; i++) {
-			data[i] = data[i+1];
-		}
-		//storePlaygroundsTitle();
-		data[AvailablePlaygrounds.playgrounds.size()] = null;
-		//storePlaygroundsTitle();
+		storePlaygroundsTitle();
+		
 		/*String owner = AvailablePlaygrounds.playgrounds.get(index-1).getOwnerEmail();
 		for (int i = 0 ; i < Account.Owners.size() ; i++) {
 			if (Account.Owners.get(i).getName().contentEquals(owner)) {
@@ -114,8 +110,9 @@ public class Administrator extends User implements ActionListener, MouseListener
 	 * This function to store playgrounds names into 'data' array
 	 */
 	private void storePlaygroundsTitle() {
+		data.clear();
 		for (int i = 0 ; i < AvailablePlaygrounds.playgrounds.size() ; i++) {
-			data[i] = String.valueOf(i+1) + ") " + AvailablePlaygrounds.playgrounds.get(i).getName();
+			data.add(String.valueOf(i+1) + ") " + AvailablePlaygrounds.playgrounds.get(i).getName());
 		}
 	}
 	
@@ -125,105 +122,105 @@ public class Administrator extends User implements ActionListener, MouseListener
 	private void viewAllPlaygrounds() {
 		JList list = new JList(data);
 		
-		panel.setVisible(false);
-		panel3.setVisible(false);
-		panel2.setVisible(true);
-		panel2.setBackground(Color.DARK_GRAY);
-		panel2.setLayout(null);
+		adminHome.setVisible(false);
+		adminPlayground.setVisible(false);
+		adminAllPlaygrounds.setVisible(true);
+		adminAllPlaygrounds.setBackground(new java.awt.Color(105,105,105));
+		adminAllPlaygrounds.setLayout(null);
 		
-		label.setText("Playgrounds");
-		label.setBounds(70, 50, 150, 50);
+		header.setText("Playgrounds");
+		header.setBounds(70, 50, 150, 50);
 		
-		back1.setBounds(5, 5, 50, 30);
-		back1.setFont (back1.getFont ().deriveFont (10f));
-		back1.setBackground(Color.ORANGE);
-		back1.setForeground(Color.DARK_GRAY);
+		adminBack1.setBounds(5, 5, 50, 30);
+		adminBack1.setFont (adminBack1.getFont ().deriveFont (10f));
+		adminBack1.setBackground(new Color(255,134,0));
+		adminBack1.setForeground(Color.WHITE);
 		
-		button2.setBackground(Color.ORANGE);
-		button2.setForeground(Color.DARK_GRAY);
-		button2.setBounds(200, 375, 80, 30);
+		goButton.setBackground(new Color(255,134,0));
+		goButton.setForeground(Color.WHITE);
+		goButton.setBounds(200, 375, 80, 30);
 		
-		label2.setBounds(10, 375, 80, 30);
-		label2.setFont (label2.getFont ().deriveFont (10f));
-		label2.setForeground(Color.WHITE);
+		PgNumLabel.setBounds(10, 375, 80, 30);
+		PgNumLabel.setFont (PgNumLabel.getFont ().deriveFont (10f));
+		PgNumLabel.setForeground(Color.WHITE);
 		
-		text.setText("");
-		text.setBounds(100, 375, 80, 30);
+		PgNumText.setText("");
+		PgNumText.setBounds(100, 375, 80, 30);
 		
 		list.setBackground(Color.WHITE);
 		list.setForeground(Color.black);
 		list.setBounds(40, 130, 200, 200);
 		
-		frame.add(panel2);
-		panel2.add(back1);
-		panel2.add(label);
-		panel2.add(label2);
-		panel2.add(button2);
-		panel2.add(text);
-		panel2.add(list);
+		frame.add(adminAllPlaygrounds);
+		adminAllPlaygrounds.add(adminBack1);
+		adminAllPlaygrounds.add(header);
+		adminAllPlaygrounds.add(PgNumLabel);
+		adminAllPlaygrounds.add(goButton);
+		adminAllPlaygrounds.add(PgNumText);
+		adminAllPlaygrounds.add(list);
 		
-		back1.addActionListener(this);
-		button2.addActionListener(this);
+		adminBack1.addActionListener(this);
+		goButton.addActionListener(this);
 	}
 	
 	/**
 	 * @param index is the index of the playground which the admin wants to view
 	 */
 	private void viewPlayground(int index) {
-		panel2.setVisible(false);
-		panel3.setVisible(true);
-		panel3.setBackground(Color.DARK_GRAY);
-		panel3.setLayout(null);
+		adminAllPlaygrounds.setVisible(false);
+		adminPlayground.setVisible(true);
+		adminPlayground.setBackground(new java.awt.Color(105,105,105));
+		adminPlayground.setLayout(null);
 		
-		label.setText(AvailablePlaygrounds.playgrounds.get(index-1).getName());
-		label.setBounds(10, 50, 250, 50);
+		header.setText(AvailablePlaygrounds.playgrounds.get(index-1).getName());
+		header.setBounds(10, 50, 250, 50);
 		
-		back2.setBounds(5, 5, 50, 30);
-		back2.setFont (back2.getFont ().deriveFont (10f));
-		back2.setBackground(Color.ORANGE);
-		back2.setForeground(Color.DARK_GRAY);
+		adminBack2.setBounds(5, 5, 50, 30);
+		adminBack2.setFont (adminBack2.getFont ().deriveFont (10f));
+		adminBack2.setBackground(new Color(255,134,0));
+		adminBack2.setForeground(Color.WHITE);
 		
-		button3.setBackground(Color.ORANGE);
-		button3.setForeground(Color.DARK_GRAY);
-		button3.setBounds(200, 375, 80, 30);
+		deleteButton.setBackground(new Color(255,134,0));
+		deleteButton.setForeground(Color.WHITE);
+		deleteButton.setBounds(200, 375, 80, 30);
 		
-		label3.setText("Location: " + AvailablePlaygrounds.playgrounds.get(index-1).getLocation());
-		label3.setBounds(40, 110, 250, 20);
-		label3.setFont (label3.getFont ().deriveFont (12f));
-		label3.setForeground(Color.WHITE);
+		locationLabel.setText("Location: " + AvailablePlaygrounds.playgrounds.get(index-1).getLocation());
+		locationLabel.setBounds(40, 110, 250, 20);
+		locationLabel.setFont (locationLabel.getFont ().deriveFont (12f));
+		locationLabel.setForeground(Color.WHITE);
 		
-		label4.setText("Size: " + String.valueOf(AvailablePlaygrounds.playgrounds.get(index-1).getSize()));
-		label4.setBounds(40, 150, 250, 20);
-		label4.setFont (label4.getFont ().deriveFont (12f));
-		label4.setForeground(Color.WHITE);
+		sizeLabel.setText("Size: " + String.valueOf(AvailablePlaygrounds.playgrounds.get(index-1).getSize()));
+		sizeLabel.setBounds(40, 150, 250, 20);
+		sizeLabel.setFont (sizeLabel.getFont ().deriveFont (12f));
+		sizeLabel.setForeground(Color.WHITE);
 		
-		label5.setText("Description: " + AvailablePlaygrounds.playgrounds.get(index-1).getDescription());
-		label5.setBounds(40, 190, 250, 20);
-		label5.setFont (label5.getFont ().deriveFont (12f));
-		label5.setForeground(Color.WHITE);
+		descriptionLabel.setText("Description: " + AvailablePlaygrounds.playgrounds.get(index-1).getDescription());
+		descriptionLabel.setBounds(40, 190, 250, 20);
+		descriptionLabel.setFont (descriptionLabel.getFont ().deriveFont (12f));
+		descriptionLabel.setForeground(Color.WHITE);
 		
-		label6.setText("Price per hour: " + String.valueOf(AvailablePlaygrounds.playgrounds.get(index-1).getPricePerHour()));
-		label6.setBounds(40, 230, 250, 20);
-		label6.setFont (label6.getFont ().deriveFont (12f));
-		label6.setForeground(Color.WHITE);
+		priceLabel.setText("Price per hour: " + String.valueOf(AvailablePlaygrounds.playgrounds.get(index-1).getPricePerHour()));
+		priceLabel.setBounds(40, 230, 250, 20);
+		priceLabel.setFont (priceLabel.getFont ().deriveFont (12f));
+		priceLabel.setForeground(Color.WHITE);
 		
-		label7.setText("Cancellation period: " + String.valueOf(AvailablePlaygrounds.playgrounds.get(index-1).getCancellationPeriod()));
-		label7.setBounds(40, 270, 250, 20);
-		label7.setFont (label7.getFont ().deriveFont (12f));
-		label7.setForeground(Color.WHITE);
+		cancellationLabel.setText("Cancellation period: " + String.valueOf(AvailablePlaygrounds.playgrounds.get(index-1).getCancellationPeriod()));
+		cancellationLabel.setBounds(40, 270, 250, 20);
+		cancellationLabel.setFont (cancellationLabel.getFont ().deriveFont (12f));
+		cancellationLabel.setForeground(Color.WHITE);
 		
-		frame.add(panel3);
-		panel3.add(back2);
-		panel3.add(label);
-		panel3.add(label3);
-		panel3.add(label4);
-		panel3.add(label5);
-		panel3.add(label6);
-		panel3.add(label7);
-		panel3.add(button3);
+		frame.add(adminPlayground);
+		adminPlayground.add(adminBack2);
+		adminPlayground.add(header);
+		adminPlayground.add(locationLabel);
+		adminPlayground.add(sizeLabel);
+		adminPlayground.add(descriptionLabel);
+		adminPlayground.add(priceLabel);
+		adminPlayground.add(cancellationLabel);
+		adminPlayground.add(deleteButton);
 		
-		back2.addActionListener(this);
-		button3.addActionListener(this);
+		adminBack2.addActionListener(this);
+		deleteButton.addActionListener(this);
 	}
 	
 	/**
@@ -231,26 +228,35 @@ public class Administrator extends User implements ActionListener, MouseListener
 	 */
 	public void actionPerformed(ActionEvent e) {
 		
-		if (e.getSource() == button) {
+		if (e.getSource() == aPlaygroundsButton) {
 			storePlaygroundsTitle();
 			viewAllPlaygrounds();
 		}
-		if (e.getSource() == button2) {
-			String str = text.getText();
+		if (e.getSource() == goButton) {
+			String str = PgNumText.getText();
 			int i = Integer.parseInt(str);
 			if (i>0 && i<=AvailablePlaygrounds.playgrounds.size()) {
 				viewPlayground(i);
 			}
 		}
-		if (e.getSource() == button3) {
-			String str = text.getText();
-			int i = Integer.parseInt(str);
-			deletePlayground(i);
+		if (e.getSource() == deleteButton) {
+			String str = PgNumText.getText();
+			int i;
+			try {
+				i = Integer.parseInt(str);
+				if (i>0 && i<=AvailablePlaygrounds.playgrounds.size()) {
+					deletePlayground(i);
+				}
+				viewAllPlaygrounds();
+			}
+			catch (NumberFormatException e1) {
+				viewAllPlaygrounds();
+			}
 		}
-		if (e.getSource() == back1) {
+		if (e.getSource() == adminBack1) {
 			displayWelcome();
 		}
-		else if (e.getSource() == back2) {
+		else if (e.getSource() == adminBack2) {
 			viewAllPlaygrounds();
 		}
 		
