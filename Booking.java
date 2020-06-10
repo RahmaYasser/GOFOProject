56c4f19756038fa98d58967a0781170154aac495
RahmaYasser <49435053+RahmaYasser@users.noreply.github.com>
2020-06-10 21:52:49 +0200

    Update Booking.java

--- Booking.java
+++ Booking.java
@@ -4,11 +4,29 @@ import javax.swing.Timer;
 
 public class Booking {
     Item item;
-    eWallet bookingWallet = new eWallet();
-    //Timer timer;
+    eWallet bookingWallet ;
+    Timer timer;
     Double Price;
+    int seconds;
+
+    @Override
+    public String toString() {
+        return "Playground name:" + item.name + "\n" +
+                "location: " + item.location + "\n" +
+                "Description: " + item.description + "\n" +
+                "Size: " + item.size +"\n"+
+                "Price per hour: " + item.pricePerHour +"\n"+
+                "Total price: " + Price+"\n"+
+                "Cancellation period " + String.valueOf(item.cancellationPeriod) +"minutes"+"\n"+
+                "Available on: " + item.timeSlot.getDay() +" from "+String.valueOf(item.timeSlot.getStartTime()) +" to "+ String.valueOf(item.timeSlot.getEndTime())+"\n";
+
+    }
 
     Booking(){
+        seconds=1;
+        item = new Item();
+        bookingWallet= new eWallet();
         bookingWallet.setCurrentMoney(0);
+
     }
 }
